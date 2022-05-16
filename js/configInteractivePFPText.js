a7e2895187c642e1ac97423f2e54b04b51157878
Roshanell <96213223+Roshanell@users.noreply.github.com>
2022-05-15 21:25:03 -0500

    First Commit

---
+++ js/configInteractivePFPText.js
@@ -0,0 +1,26 @@
+(function () {
+    // When you click on your PFP, your PFP will interact and say something. Make a list for it to say!
+    let textEntries = ["Hi there, click me!",
+        "do it again!",
+        "*clears throat*",
+        "Hi! I'm Roshanell!",
+        "I'm an educator turned software engineer.",
+        "I've been in tech for half a year,",
+        "building awesome websites and software with 100Devs Agency.",
+        "Nice to meet you, and let's connect!",
+        "Ask me how an educator would benefit your company.",
+    ];
+
+    let index = 0;
+    const bubbleTextElement = document.querySelector(".mainTextBox");
+    bubbleTextElement.style.display = "block";
+    function nextTextForBubble() {
+        textEntries[index] && (bubbleTextElement.innerText = textEntries[index]);
+        index++;
+    }
+    nextTextForBubble();
+    
+    Array.from(document.querySelectorAll(".mainIconContainer *")).forEach(i=>i.addEventListener("click", function (e) {
+        nextTextForBubble();
+    }));
+}());
